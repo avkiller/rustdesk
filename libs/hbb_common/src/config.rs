ed3aed69409caed35f462614a6434da61b812664
avkiller <avkiller2012@gmail.com>
2024-11-28 22:45:10 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -774,8 +774,8 @@ impl Config {
                 .unwrap_or_default();
         }
         if !rendezvous_server.contains(':') {
-            //let port = RENDEZVOUS_PORT.read().unwrap(); // 获取读锁
-            rendezvous_server = format!("{rendezvous_server}:{*RENDEZVOUS_PORT}");
+            let port = *RENDEZVOUS_PORT // 获取读锁
+            rendezvous_server = format!("{rendezvous_server}:{port}");
             
         }
         rendezvous_server

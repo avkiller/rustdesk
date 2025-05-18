8957f45b0a39ae36bd8bd6bd57b2bc14ab6beda9
avkiller <avkiller2012@gmail.com>
2025-05-18 02:10:59 +0000

    同步代码

--- src/common.rs
+++ src/common.rs
@@ -964,8 +964,8 @@ pub fn get_api_server(api: String, custom: String) -> String {
     if res.ends_with('/') {
         res.pop();
     }
-    if res.starts_with("https") && res.ends_with(":21114") {
-        return res.replace(":21114", "");
+    if res.starts_with("https") && res.ends_with(":10084") {
+        return res.replace(":10084", "");
     }
     res
 }
@@ -993,7 +993,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://rs.fuxudong.com".to_owned()
 }
 
 #[inline]
